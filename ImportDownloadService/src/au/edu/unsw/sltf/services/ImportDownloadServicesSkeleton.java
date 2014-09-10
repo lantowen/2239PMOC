324c117a1e76c480accd787ed71a89db672a7d13
lantowen <lantian.unsw@gmail.com>
2014-09-10 18:07:35 +1000

    second commit

--- ImportDownloadService/src/au/edu/unsw/sltf/services/ImportDownloadServicesSkeleton.java
+++ ImportDownloadService/src/au/edu/unsw/sltf/services/ImportDownloadServicesSkeleton.java
@@ -8,11 +8,17 @@
 package au.edu.unsw.sltf.services;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
+
+import org.apache.axis2.description.AxisService;
+
+import com.sun.corba.se.spi.activation.Server;
 
 import au.edu.unsw.sltf.services.ImportDownloadFaultDocument.ImportDownloadFault;
 import au.edu.unsw.sltf.services.ImportMarketDataDocument.ImportMarketData;
@@ -39,20 +45,11 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
 		ImportMarketData req = reqDoc.getImportMarketData();
 		String secStr = req.getSec();
 		String urlStr = req.getDataSourceURL();
-		Calendar StartDate = req.getStartDate();
-		Calendar EndDate = req.getEndDate();
-		URL url;
-		try	{
-			 url = new URL(urlStr);
-		} catch (MalformedURLException mue) {
-			ImportDownloadFaultException e = new ImportDownloadFaultException();
-			ImportDownloadFaultDocument fDoc = ImportDownloadFaultDocument.Factory.newInstance();
-			ImportDownloadFault f = fDoc.addNewImportDownloadFault();
-			f.setFaultMessage("Invalid URL: " + urlStr);
-			f.setFaultType(ImportDownloadFaultType.INVALID_URL);
-			e.setFaultMessage(fDoc);
-			throw e;
-		}
+		Calendar startDate = req.getStartDate();
+		Calendar endDate = req.getEndDate();
+		URL url = null;
+		String fileName;
+		boolean urlFlag = false;
 		if (!secStr.matches("[A-Z]{3,4}")) {
 
 			ImportDownloadFaultException e = new ImportDownloadFaultException();
@@ -63,16 +60,70 @@ public class ImportDownloadServicesSkeleton implements ImportDownloadServicesSke
 			e.setFaultMessage(fDoc);
 			throw e;
 		}
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-			String inputLine;
-	        while ((inputLine = in.readLine()) != null)
-	            System.out.println(inputLine);
-	        in.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();	
+		if(startDate.after(endDate)) {
+			ImportDownloadFaultException e = new ImportDownloadFaultException();
+			ImportDownloadFaultDocument fDoc = ImportDownloadFaultDocument.Factory.newInstance();
+			ImportDownloadFault f = fDoc.addNewImportDownloadFault();
+			f.setFaultMessage("Invalid date");
+			f.setFaultType(ImportDownloadFaultType.PROGRAM_ERROR);
+			e.setFaultMessage(fDoc);
+			throw e;
 		}
+		do {
+			try	{
+				url = new URL(urlStr);
+				if(!url.getProtocol().toLowerCase().equals("http")) {
+					urlFlag = true;
+					break;
+				}
+			} catch (MalformedURLException mue) {
+				urlFlag = true;
+				break;
+			}
+			fileName = url.toString().substring(url.toString().lastIndexOf("/")+1);
+			System.out.println(fileName);
+			if(fileName.equals("")) {
+				urlFlag = true;
+				break;
+			}
+			else if(!fileName.substring(fileName.lastIndexOf(".")+1).equals("csv")) {
+				urlFlag = true;
+				break;
+			}
+			else {
+					fileName = fileName.substring(0,fileName.lastIndexOf("."))+System.currentTimeMillis()
+							+fileName.substring(fileName.lastIndexOf("."));
+				try {
+					BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+					String inputLine;
+					String path = System.getenv("CATALINA_HOME");
+					boolean dir = new File(path+"/temp/").mkdir();
+					if(dir == false) {
+						System.out.println("failed to create a temp directory"+path+"/temp/");
+					}
+					PrintWriter writer = new PrintWriter(path+"/temp/"+fileName, "UTF-8");
+					inputLine = in.readLine();
+			        while ((inputLine = in.readLine()) != null)
+			           writer.println(inputLine);
+			        in.close();
+			        writer.close();
+				} catch (IOException e) {
+					urlFlag = true;
+				}
+			}
+		} while(false);
+		
+		if(urlFlag == true) {
+			ImportDownloadFaultException e = new ImportDownloadFaultException();
+			ImportDownloadFaultDocument fDoc = ImportDownloadFaultDocument.Factory.newInstance();
+			ImportDownloadFault f = fDoc.addNewImportDownloadFault();
+			f.setFaultMessage("Invalid URL: " + urlStr);
+			f.setFaultType(ImportDownloadFaultType.INVALID_URL);
+			e.setFaultMessage(fDoc);
+			throw e;
+		}
+
+		
 		System.out.println(secStr);
 		System.out.println(urlStr);
 		System.out.println(StartDate);
