a0f6b91af70aeb8e7d60ad7f7c96fcf11f6618cb
mathew <mathewchong7@gmail.com>
2014-09-10 19:30:31 +1000

    added autogen stub for currency convert

---
+++ CurrencyConvertService/src/au/edu/unsw/sltf/services/CurrencyConvertServiceSkeletonInterface.java
@@ -0,0 +1,30 @@
+
+/**
+ * CurrencyConvertServiceSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package au.edu.unsw.sltf.services;
+    /**
+     *  CurrencyConvertServiceSkeletonInterface java skeleton interface for the axisService
+     */
+    public interface CurrencyConvertServiceSkeletonInterface {
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param currencyConvertMarketData
+             * @throws CurrencyConvertFaultException : 
+         */
+
+        
+                public au.edu.unsw.sltf.services.CurrencyConvertMarketDataResponseDocument currencyConvertMarketData
+                (
+                  au.edu.unsw.sltf.services.CurrencyConvertMarketDataDocument currencyConvertMarketData
+                 )
+            throws CurrencyConvertFaultException;
+        
+         }
+    
