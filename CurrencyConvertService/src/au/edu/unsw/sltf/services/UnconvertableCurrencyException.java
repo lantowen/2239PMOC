02a57c910f7e115e78aedae2857ea26fae554c2e
mathew <mathewchong7@gmail.com>
2014-09-11 20:51:15 +1000

    added class to do currency conversion

---
+++ CurrencyConvertService/src/au/edu/unsw/sltf/services/UnconvertableCurrencyException.java
@@ -0,0 +1,13 @@
+package au.edu.unsw.sltf.services;
+
+public class UnconvertableCurrencyException extends Exception {
+	
+	public UnconvertableCurrencyException() {
+		super();
+	}
+
+	public UnconvertableCurrencyException(String message) {
+		super(message);
+	}
+
+}
